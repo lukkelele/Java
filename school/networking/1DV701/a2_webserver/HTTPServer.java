25d6c22f4cff65db124f420077ee3687fae4ff68
Lukas Gunnarsson <encryptedluke@protonmail.com>
2022-02-17 14:49:25 +0100

    Runnable interface getting implemented

--- school/networking/1DV701/a2_webserver/HTTPServer.java
+++ school/networking/1DV701/a2_webserver/HTTPServer.java
@@ -4,10 +4,49 @@ import java.util.Scanner;
 
 public class HTTPServer {
 
-  static int port;
-  static String path = "";
+  private static int port;
+  private static String path = "";
+  private Socket connection;
+
+
 
   public HTTPServer(Socket s) {
+    connection = s;                   
+  }
+
+
+
+  public static void main(String[] args) implements Runnable {
+    try {   
+      Scanner user = new Scanner(System.in);
+      int c = boot(args);
+      switch(c) {
+
+        case 0:
+          // FIX FAULTY ARGUMENTS
+          String input = "";
+          boolean arg_flag = false;
+          while (arg_flag == false) {
+            try {
+            input = user.nextLine();
+            arg_flag = checkPortArg(input);
+            } catch (Exception e) {}
+          } 
+          port = Integer.parseInt(input);
+          user.close();
+        case 1:
+          // START SERVER
+          System.out.println("Starting server on port "+port+"...");
+          ServerSocket serverConnection = new ServerSocket(port);
+          serverConnection.accept();    // Listen to chosen port and accept incoming connection
+          HTTPServer server = new HTTPServer(serverConnection);
+          
+
+          break;
+        }
+    } catch (Exception e) {
+      System.out.println("Error: RUNNABLE TRY/CATCH CLAUSE\n"+e);
+    }
   }
 
 
@@ -18,41 +57,20 @@ public class HTTPServer {
   }
 
 
-  // Take command line arguments, first PORT then relative path
-  public static void main(String[] args) {
-    // first  arg --> port  --> parseInteger 
-    // second arg --> relative path --> String
-    Scanner user = new Scanner(System.in);
+  static int boot(String[] args) {
     int c = 0;
     boolean start = startup(args);
     if (start == true) {
-      c = 1;
+      c = 1;        // Indicates that server is ready to start
       port = Integer.parseInt(args[0]);
     };
 
-    switch(c) {
-      case 0:
-        // FIX ARGUMENTS
-        String input = "";
-        boolean arg_flag = false;
-        while (arg_flag == false) {
-          try {
-          input = user.nextLine();
-          arg_flag = checkPortArg(input);
-          } catch (Exception e) {}
-        } 
-        port = Integer.parseInt(input);
-      case 1:
-        // START SERVER
-        System.out.println("Starting server on port "+port+"...");
-        break;
-    }
-
+    return c;
   }
 
 
   static boolean checkPortArg(String arg) {
-    System.out.println("CHECKING PORT ARGUMENT");
+    System.out.println("Checking validity of port argument..");
     try {
       Integer.parseInt(arg);
       System.out.println("ARGUMENT VALID!");
