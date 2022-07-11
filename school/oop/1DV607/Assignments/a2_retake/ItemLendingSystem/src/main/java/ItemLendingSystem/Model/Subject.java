cb8ac836d9ccceb8dd55f3a75d43bac0cf64994e
lukkelele <encryptedluke@protonmail.com>
2022-07-11 14:00:04 +0200

    upd

--- school/oop/1DV607/Assignments/a2_retake/ItemLendingSystem/src/main/java/ItemLendingSystem/Model/Subject.java
+++
@@ -1,7 +0,0 @@
-package ItemLendingSystem.Model;
-
-public interface Subject {
-    public void update(int days);
-    public void signContract(Observer subscriber);
-    public void ceaseContract(Observer subscriber);
-}
